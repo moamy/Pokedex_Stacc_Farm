bb0921deaf752a78cce43655b8d2dead2512083f
moamy <moamyrs@gmail.com>
2025-09-11 22:25:08 +0200

    Installed Playwright
    Installed Playwright through VS Code extension

---
+++ tests/pokedex.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('submits form', async ({ page }) => {
+
+});
+
+test('signals failure on failed search', async ({ page }) => {
+
+});
+
+test('displays pokemon on successful search', async ({ page }) => {
+
+});
+
+test('re-animates input after two failures in a row', async ({ page }) => {
+
+});
