0d6b72b07f1f5298de0050694bb58c84b8a54703
moamy <moamyrs@gmail.com>
2025-09-12 20:46:12 +0200

    formatted code Prettier (VS Extension)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // An animatiion for signaling failure
 const shake = [
-    { transform: "translateX(0%)"},
-    { transform: "translateX(10%)"},
-    { transform: "translateX(-10%)"}
+    { transform: "translateX(0%)" },
+    { transform: "translateX(10%)" },
+    { transform: "translateX(-10%)" }
 ];
 // Declare (and define) global variables
 const textInput = document.getElementById("name");
@@ -13,8 +13,8 @@ let previousResults = [];
 let matchFound;
 
 // Checks the cache before deciding if an API lookup is necessary
-function checkCache(pokemonName){
-    console.log("storage: "+ localStorage.getItem("cachedPokémon"));
+function checkCache(pokemonName) {
+    console.log("storage: " + localStorage.getItem("cachedPokémon"));
     // Disables text input to communicate a loading process and prevent 
     // the user from changing their input in the meanwhile. 
     textInput.disabled = true;
@@ -23,32 +23,32 @@ function checkCache(pokemonName){
     console.log(previousResults);
     // If the cache is not empty, check to see if the requested pokemon 
     // happens to be saved in local storage.
-    if(!previousResults || previousResults.length != 0){
+    if (!previousResults || previousResults.length != 0) {
         previousResults.forEach(element => {
-                if(userInput == element.name){
-                    // If the pokemon is found in the cache, 
-                    // display results using cache. 
-                    displayResult(element);
-                    matchFound = true;
-                    console.log("Match found!!");
-                }
+            if (userInput == element.name) {
+                // If the pokemon is found in the cache, 
+                // display results using cache. 
+                displayResult(element);
+                matchFound = true;
+                console.log("Match found!!");
+            }
         });
     }
     // If no match was found, 
-    if(!matchFound){
+    if (!matchFound) {
         fetchAPI(userInput);
     }
 }
 // Takes the user input to fetch a specific pokémon with the API
-async function fetchAPI(userInput){
+async function fetchAPI(userInput) {
     let url = "https://pokeapi.co/api/v2/pokemon/" + userInput + "/";
     // Resets the text input color
     textInput.style.backgroundColor = "#fff";
     // Tries to fetch API
-    try { 
+    try {
         const response = await fetch(url);
         // Signals a negative response upon recieving a 404 and throws error
-        if (!response.ok){
+        if (!response.ok) {
             displayResult(null);
             throw new Error('Response status: ${response.status}');
         }
@@ -59,23 +59,23 @@ async function fetchAPI(userInput){
         // Caches the result
         cacheResult(result);
         // Catches any errors for debugging purposes
-        } catch (error) {
-            console.error(error.message);
-        }
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 // Displays a pokémon sprite or feedback depending on the results
-function displayResult(pokemon){
+function displayResult(pokemon) {
     textInput.disabled = false;
 
     // If the fetch was successful, display the sprite
-    if(pokemon){
+    if (pokemon) {
         img.src = pokemon.sprites.front_default;
         textInput.style.backgroundColor = "#fff";
         figcaption.textContent = "#" + pokemon.id;
         searchResult.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-    // Otherwise, communicate the failure through an animation and imagery. 
-    }else{
+        // Otherwise, communicate the failure through an animation and imagery. 
+    } else {
         img.src = "/cross.png";
         textInput.style.animation = 'none';
         textInput.style.backgroundColor = "#ff9f94";
@@ -87,23 +87,23 @@ function displayResult(pokemon){
 }
 
 // Tries to cache results. Deletes the cache upon failed attempt.
-function cacheResult(result){
+function cacheResult(result) {
     try {
         previousResults.push(result);
         localStorage.setItem("cachedPokémon", previousResults);
-    } catch (e){
+    } catch (e) {
         removeCache();
     }
 }
 
 // Removes cache
-function removeCache(){
+function removeCache() {
     previousResults = [];
     localStorage.clear();
 }
 
 // Calls the removal of cache before closing or reloading the website.
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
     removeCache();
     return null;
 }
--- tests/pokedex.spec.ts
+++ tests/pokedex.spec.ts
@@ -1,48 +1,54 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect, Page } from "@playwright/test";
 
-
-test('signals failure on failed search', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/');
-    await page.locator('#name').click();
-    // Fills the text input with something that won't result in a match
-    await page.locator('#name').fill('null');
-    await page.getByRole('button', { name: 'Submit' }).click();
-    // Makes sure the headings are showing the correct text
-    await expect(page.locator('#searchResult')).toHaveText("Not found!");
-    await expect(page.locator('#pokemonId')).toHaveText("Try searching for another pokémon.");
-
-    // Makes sure the input turns bright red/pink
-    await expect(page.locator('#name')).toHaveCSS('background-color', 'rgb(255, 159, 148)');
-
-    // Makes sure the picture source changes to a red cross
-    await expect(page.getByRole('img', { name: 'Picture displaying a pokémon.' })).toHaveScreenshot('cross.png');
+test("signals failure on failed search", async ({ page }) => {
+  await page.goto("http://127.0.0.1:5500/");
+  await page.locator("#name").click();
+  // Fills the text input with something that won't result in a match
+  await page.locator("#name").fill("null");
+  await page.getByRole("button", { name: "Submit" }).click();
+  // Makes sure the headings are showing the correct text
+  await expect(page.locator("#searchResult")).toHaveText("Not found!");
+  await expect(page.locator("#pokemonId")).toHaveText(
+    "Try searching for another pokémon."
+  );
+  // Makes sure the input turns bright red/pink
+  await expect(page.locator("#name")).toHaveCSS(
+    "background-color",
+    "rgb(255, 159, 148)"
+  );
+  // Makes sure the picture source changes to a red cross
+  await expect(
+    page.getByRole("img", { name: "Picture displaying a pokémon." })
+  ).toHaveScreenshot("cross.png");
 });
 
 // Makes sure searching for a pokemon (clefairy) will result in displaying its sprite
-test('displays pokemon on successful search', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/');
-    await page.locator('#name').click();
-    await page.locator('#name').fill('clefairy');
-    await page.getByRole('button', { name: 'Submit' }).click();
-    // Does not check the whole process of getting a response from the API,
-    // only checks the src of the image to include a part of the APIs image folder path. 
-    await expect(page.getByRole('img', { name: 'Picture displaying a pokémon.' })).toHaveAttribute('src', /\PokeAPI\/sprites/);
+test("displays pokemon on successful search", async ({ page }) => {
+  await page.goto("http://127.0.0.1:5500/");
+  await page.locator("#name").click();
+  await page.locator("#name").fill("clefairy");
+  await page.getByRole("button", { name: "Submit" }).click();
+  // Does not check the whole process of getting a response from the API,
+  // only checks the src of the image to include a part of the APIs image folder path.
+  await expect(
+    page.getByRole("img", { name: "Picture displaying a pokémon." })
+  ).toHaveAttribute("src", /\PokeAPI\/sprites/);
 });
 
 // Makes sure the API results in an OK response
-test('gets OK response from API', async ({ page }) => {
-    await page.goto('http://127.0.0.1:5500/');
-    await page.locator('#name').click();
-    await page.locator('#name').fill('pikachu');
+test("gets OK response from API", async ({ page }) => {
+  await page.goto("http://127.0.0.1:5500/");
+  await page.locator("#name").click();
+  await page.locator("#name").fill("pikachu");
 
-    // Waits for a response from the API and identifies it via a part of the URL
-    const responsePromise = page.waitForResponse(response =>
-        response.url().includes("pokeapi.co/api/v2/pokemon/")
-    );
+  // Waits for a response from the API and identifies it via a part of the URL
+  const responsePromise = page.waitForResponse((response) =>
+    response.url().includes("pokeapi.co/api/v2/pokemon/")
+  );
 
-    await page.getByRole('button', { name: 'Submit' }).click();
-
-    const response = await responsePromise;
-    // If the response is 200 (OK) then the test will be passed
-    await expect(response.status() === 200).toBeTruthy;
+  await page.getByRole("button", { name: "Submit" }).click();
+  
+  const response = await responsePromise;
+  // If the response is 200 (OK) then the test will be passed
+  await expect(response.status() === 200).toBeTruthy;
 });
